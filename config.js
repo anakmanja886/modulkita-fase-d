7e2a053d6f6cae6fc166ee0f6059d5b21b712f9b
anakmanja886 <anakmanja886@gmail.com>
2026-09-23 16:29:49 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+window.MODULKITA_CONFIG = {
+  supabaseUrl: "https://zpdcgqxqrjqrjfxnvkry.supabase.co",
+  supabasePublishableKey: "sb_publishable_D0lIeHQo2OdyNBvVmauxog_Dl4_k3sv",
+
+  brandName: "ModulKita Premium",
+  adminWhatsApp: "082395342965",
+
+  paymentMethod: "DANA",
+  danaName: "Rizky Udin",
+  danaNumber: "082395342965",
+  qrisImageUrl: "",
+
+  packages: [
+    { id: "premium_30", name: "Premium 30 Hari", price: "Rp30.000", days: 30 },
+    { id: "premium_year", name: "Premium 1 Tahun", price: "Rp120.000", days: 365 },
+    { id: "lifetime", name: "Lifetime", price: "Rp150.000", days: null }
+  ]
+};
